0e5a021e0af6cc7645d42746661f218512c7b4ef
vfloresp <vfloresp@pop-os.localdomain>
2021-03-15 14:59:49 -0600

    clases cliente y dervidor

--- proyecto_alpha/src/interfaces/Registro.java
+++ proyecto_alpha/src/interfaces/Registro.java
@@ -3,5 +3,8 @@ package interfaces;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface Registro {
+public interface Registro extends Remote{
+
+    public Player registro(String nombre) throws RemoteException;
+
 }
